f615ac2bf3d303f6856addb38bc1290c10bb8d55
Angie Corrales <95327338+Angiecore@users.noreply.github.com>
2022-03-22 15:34:12 -0700

    Add files via upload

--- Mapping_Single_Points/logic.js
+++ Mapping_Single_Points/logic.js
@@ -2,6 +2,7 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
+<<<<<<< Updated upstream
 let map = L.map("mapid", {
     center: [40.7, -94.5],
     zoom: 4
@@ -18,6 +19,19 @@ L.circle([34.0522, -118.2437], {
 
   // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+=======
+let map = L.map("mapid").setView([34.0522, -118.2437], 14);
+
+//  Add a circle to the map for Los Angeles, California.
+L.circleMarker([34.0522, -118.2437], {
+  radius: 300,
+  color: "black",
+  fillColor: "#ffffa1"
+}).addTo(map);
+
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+>>>>>>> Stashed changes
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox/streets-v11',
@@ -26,5 +40,9 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{
     accessToken: API_KEY
 });
 // Then we add our 'graymap' tile layer to the map.
+<<<<<<< Updated upstream
 streets.addTo(map);
 
+=======
+streets.addTo(map);
+>>>>>>> Stashed changes
